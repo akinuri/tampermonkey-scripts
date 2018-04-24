447b0465d0efe1f4c4b259e34ddb9c834ec57524
Noreh AD <akinoreh@gmail.com>
2018-04-24 16:12:19 +0300

    add. desc.

--- PHP.net.js
+++ PHP.net.js
@@ -4,14 +4,25 @@
 // @version      0.1
 // @description  Change language to "en"
 // @author       Noreh AD
-// @match        *php.net/*
+// @match        http://php.net/manual/*/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict';
 
-    if (location.href.match(/\/tr\//)) {
-        location.href = window.location.href.replace(/\/tr\//, "/en/");
+    // google.com.* shows documentation pages in user's native language, and for me, this is not preferable
+    // most documentations are written in English and translations do not make much sense
+    // this is usually not a translation related problem, but a language related problem
+    // every programming language (basics, concepts, etc.) I've encountered is written in English
+    // when you translate these into another language, they do not make much sense, because
+    // these concepts do not exists in the target language
+    // so I rather view the original
+
+    if (match = location.href.match(/http:\/\/php.net\/manual(\/.*?\/).*/)) {
+        var lang = match[1];
+        if (lang != "/en/") {
+            location.href = window.location.href.replace(lang, "/en/");
+        }
     }
+
 })();
